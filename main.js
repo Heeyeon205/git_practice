a379f1638a1a55e5243f542eb44b603dcf3960e4
(Lyle) <(procross27@gamil.com)>
2025-01-15 16:53:50 +0900

    feat: Add addNums function dquote> dquote> #1

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const num1 = 10;
 const num2 = 20;
 
 function addNum(num1, num2){
-    console.log('${num1} + ${num2} = &{num1+num2}');
+    console.log('${num1} + ${num2} = ${num1+num2}');
 }
 
 // 코드 러너 실행 cmd alt n
